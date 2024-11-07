4f982d1e63e919a6e8cd6ed51d1ec15c97cb4331
sid1324 <91389824+sid1324@users.noreply.github.com>
2024-11-07 23:18:36 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,40 @@
+# Code refactored from https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
+
+import openai
+import streamlit as st
+
+with st.sidebar:
+    st.title('🤖💬 Nexus 2.0 Design Assistant')
+    if 'OPENAI_API_KEY' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        openai.api_key = st.secrets['OPENAI_API_KEY']
+    else:
+        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
+        if not (openai.api_key.startswith('sk-')):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "system", "content": "You are an overly friendly UI graphic designer chatbot named DesignBuddy. Your goal is to help users create a basic UI guideline for Euclid, and you're not satisfied unless the customer is completely satisfied. Clour scheme includes pinkish red and light green. You provide code for all components in react and using nexus."})
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model="ft:gpt-3.5-turbo-0125:personal:ted:AQhSM9sI",
+            messages=[{"role": m["role"], "content": m["content"]}
+                      for m in st.session_state.messages], stream=True):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
